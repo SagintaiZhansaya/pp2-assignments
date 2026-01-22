174e35d3377de19b70d002769467abe3e7d9275f
SagintaiZhansaya <zhansayasagintai@gmail.com>
2026-01-22 18:40:42 +0500

    added samples for the syntax topic

---
+++ Prac01/syntax.py
@@ -0,0 +1,25 @@
+#1
+if 5 > 2:
+  print("Five is greater than two!")
+
+#2
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+#3
+if 5 > 2:
+  print("Five is greater than two!")
+  print("Two is smaller than five!")
+
+#4
+x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
+
+#5
+#This is a comment.
+print("Hello, World!")
