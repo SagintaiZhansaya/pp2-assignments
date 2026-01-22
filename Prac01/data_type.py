8bc5b1ff4ed7f816c0a0330c9e47a054a5c54f46
SagintaiZhansaya <zhansayasagintai@gmail.com>
2026-01-22 20:23:33 +0500

    added samples for the data types topic

---
+++ Prac01/data_type.py
@@ -0,0 +1,30 @@
+#1
+x = 5
+
+print(x)
+print(type(x))
+
+#2
+x = 20.5
+
+print(x)
+print(type(x))
+
+#3
+x = "Hello, World!"
+
+print(x)
+print(type(x))
+
+#4
+x = True
+
+print(x)
+print(type(x))
+
+#5
+x = ["apple", "banana", "cherry"]
+
+
+print(x)
+print(type(x))
