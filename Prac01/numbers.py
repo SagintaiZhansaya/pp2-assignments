435e58e9918b4b241411ae530479bced0c668221
SagintaiZhansaya <zhansayasagintai@gmail.com>
2026-01-22 20:35:57 +0500

    added examples for the numbers topic

---
+++ Prac01/numbers.py
@@ -0,0 +1,52 @@
+#1
+x = 1    
+y = 2.8  
+z = 1j   
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#2
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#3
+x = 1.10
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#4
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#5
+x = 1    
+y = 2.8  
+z = 1j   
+
+a = float(x)
+b = int(y)
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
