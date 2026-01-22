8f537f2a358a1a40ca427502c2a11a3a5654b4dc
SagintaiZhansaya <zhansayasagintai@gmail.com>
2026-01-22 21:00:08 +0500

    added examples for the strings topic

---
+++ Prac01/strings.py
@@ -0,0 +1,20 @@
+#1
+for x in "apple":
+    print(x)
+
+#2
+s = "Hello new day!"
+print("new" in s)
+
+#3
+s = "Have a good day!"
+print(s[7:11])
+
+#4
+a = "Hello, World!"
+print(a.replace("Hello", "Bye"))
+
+#5
+age = 17
+txt = f"My name is Zhansaya, I am {age}"
+print(txt)
