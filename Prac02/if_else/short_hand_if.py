5907335b79b009b763b4829c95576e3c1b2377e7
SagintaiZhansaya <zhansayasagintai@gmail.com>
2026-01-29 16:11:38 +0500

    Added examples for the short hand if

---
+++ Prac02/if_else/short_hand_if.py
@@ -0,0 +1,29 @@
+#1
+a = 6
+
+if a % 2 == 0: print("a number is even")
+
+#2
+a = 2
+b = 4
+
+print("A") if a > b else print("B")
+
+#3
+a = 10
+b = 20
+bigger = a if a > b else b
+print("Bigger is", bigger)
+
+#4
+x = 90
+y = 65
+max_value = x if x > y else y
+print("Maximum value:", max_value)
+
+#5
+weather = "sunny"
+
+today = "good weather" if weather == "sunny" else "bad weather"
+print("Today is", today)
+
