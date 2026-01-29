de9c20773e9a81e075571d4585dfce31279bc740
SagintaiZhansaya <zhansayasagintai@gmail.com>
2026-01-29 14:53:13 +0500

    Added examples for the comparison topic

---
+++ Prac02/boolean/boolean_comparison.py
@@ -0,0 +1,33 @@
+#1
+print(6 > 2)
+print(6 == 2)
+print(6 < 2)
+
+#2
+a = 5
+b = 1
+
+if a > b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")
+
+#3
+score = 60
+
+if score > 50:
+    print("You passed!")
+else:
+    print("You failed.")
+
+#4
+result = (5 < 10)
+print(result)
+
+#5
+num = 9
+
+if num == 7:
+    print("You are lucky")
+else:
+    print("Not today, good luck")
