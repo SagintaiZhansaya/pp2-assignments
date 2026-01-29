fe783edaa5d15596b156303a3700763274036dfb
SagintaiZhansaya <zhansayasagintai@gmail.com>
2026-01-29 15:22:31 +0500

    Added examples for the if statement

---
+++ Prac02/if_else/if_statement.py
@@ -0,0 +1,28 @@
+#1
+age = 20
+
+if age >= 18:
+    print("You are adult")
+
+#2
+num = 8
+
+if num > 0:
+    print("the number is positive")
+
+#3
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+
+#4
+new_year = True
+
+if new_year:
+    print("Happy new year!")
+
+#5
+place = 1
+
+if place == 1:
+    print("You won!")
