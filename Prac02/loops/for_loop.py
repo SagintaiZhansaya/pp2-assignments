74c236e2a726fc88efcb39395a7166329e2e51c1
SagintaiZhansaya <zhansayasagintai@gmail.com>
2026-01-30 08:08:13 +0500

    Added examples for the for loop

---
+++ Prac02/loops/for_loop.py
@@ -0,0 +1,23 @@
+#1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+#2
+for x in "banana":
+  print(x)
+
+#3
+for x in range(6):
+  print(x)
+
+#4
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+#5
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(i, "x", j, "=", i * j)
