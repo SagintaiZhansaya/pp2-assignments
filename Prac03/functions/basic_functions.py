e747dd3f57b30036e531bd1843d52ccf611842db
SagintaiZhansaya <zhansayasagintai@gmail.com>
2026-02-05 08:44:43 +0500

    added examples for the basic functions

---
+++ Prac03/functions/basic_functions.py
@@ -0,0 +1,30 @@
+#1
+def my_func():
+    print("hello from functon")
+
+my_func()
+
+#2
+def greating():
+    return "Hello!"
+
+print(greating())
+
+#3
+def message():
+    return "Take the ambrella"
+
+m = message()
+print(m)
+
+#4
+def addition():
+    return 3 + 4
+
+print(3, '+', 4, '=', addition())
+
+#5
+def multiplication():
+    return 5 * 6
+
+print(5, '*', 6, '=', multiplication())
