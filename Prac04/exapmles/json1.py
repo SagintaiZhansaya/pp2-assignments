64b7b6e5beb534a4fe5dbb580a368680befec953
SagintaiZhansaya <zhansayasagintai@gmail.com>
2026-02-19 10:39:26 +0500

    added examples for json

---
+++ Prac04/exapmles/json1.py
@@ -0,0 +1,75 @@
+#1
+import json
+
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+y = json.loads(x)
+
+print(y["age"])
+
+#2
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+y = json.dumps(x)
+
+print(y)
+
+#3
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x, indent=4, sort_keys=True))
+
+#4
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x, indent=4, separators=(". ", " = ")))
+
+#5
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x, indent=4))
