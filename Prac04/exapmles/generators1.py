e4df71feba99c2940148733967658cfbb24509fd
SagintaiZhansaya <zhansayasagintai@gmail.com>
2026-02-19 09:21:31 +0500

    added examples for iterator and generator

---
+++ Prac04/exapmles/generators1.py
@@ -0,0 +1,53 @@
+#1
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+#2
+mytuple = ("apple", "banana", "cherry")
+
+for x in mytuple:
+    print(x)
+
+#3
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        if self.a <= 20:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+    print(x)
+
+#4
+def fun(max):
+    cnt = 1
+    while cnt <= max:
+        yield cnt
+        cnt += 1
+
+ctr = fun(5)
+for n in ctr:
+    print(n)
+
+#5
+def even_num(n):
+    for i in range(n):
+        if i % 2 == 0:
+            yield i
+
+for num in even_num(10):
+    print(num)
