7257fa0dcd3903218aa50b3925938243409e314e
SagintaiZhansaya <zhansayasagintai@gmail.com>
2026-02-19 09:52:13 +0500

    added examples for math

---
+++ Prac04/exapmles/math1.py
@@ -0,0 +1,31 @@
+#1
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+#2
+x = abs(-7.25)
+
+print(x)
+
+#3
+import math
+
+x = math.sqrt(64)
+
+print(x)
+
+#4
+import random
+
+z = random.randint(1, 6)
+print(z)
+
+#5
+import random
+
+colors = ['red', 'green', 'blue', 'yellow']
+picked_color = random.choice(colors)
+print(picked_color)
